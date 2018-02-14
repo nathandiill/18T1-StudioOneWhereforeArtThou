ead95aec6227b37cfdf527df42c09fefa8dc5525
NATMAN-LAPTOP <1004946@student.sae.edu.au>
2018-02-14 14:51:29 +1000

    Fix falling bug

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+	void Start ()
+    {
+        // Retrieve the Ridgidbody from gameObject
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	void Update ()
+    {
+          	
+	}
+}
